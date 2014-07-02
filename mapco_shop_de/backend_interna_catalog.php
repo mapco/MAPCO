414bfa44288ad5c9e9ec59c2b7a75e8b607be7cf
mapco <rlange@mapco.de>
2014-07-02 17:13:12 +0200

    create pdf catalog begin

--- mapco_shop_de/backend_interna_catalog.php
+++ mapco_shop_de/backend_interna_catalog.php
@@ -5,7 +5,27 @@ include("config.php");
 include("templates/" . TEMPLATE_BACKEND . "/header.php");
 
 //	Content
+?>
+<script type="text/javascript">
 
+	/**
+     * show
+     */
+	function showCatalog()
+    {
+        var $post_data = new Object();
+        $post_data['API'] = "catalog";
+        $post_data['APIRequest'] = "CatalogCreate";
+        $post_data['action'] = "showCatalog";
+        $.post('<?php echo PATH;?>soa2/', $post_data, function($data) {
+            wait_dialog_show();
+            try {$xml = $($.parseXML($data));} catch($err) {show_status2($err.message); return;}
+            if ($xml.find('Ack').text() != 'Success') {show_status2($data); return;}
+        });
+    }
+</script>
+
+<?php
 //	Breadcrumbs
 echo '
 	<div id="breadcrumbs" class="breadcrumbs">
@@ -16,4 +36,5 @@ echo '
 	<h1>Catalog - Dashboard</h1>';
 
 //	Footer
+echo '<script type="text/javascript">showCatalog(' . $get['orderBy'] . ');</script>';
 include("templates/" . TEMPLATE_BACKEND . "/footer.php");
