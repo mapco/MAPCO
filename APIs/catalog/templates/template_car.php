af2003c818b28f1b2ec901755107cced5c855065
mapco <rlange@mapco.de>
2014-07-04 08:55:57 +0200

    add vehicles view, fix add commit

---
+++ APIs/catalog/templates/template_car.php
@@ -0,0 +1,19 @@
+<?php
+
+
+$templateCar = '
+
+	<table>
+		<thead>
+			<tr>
+				<th colspan="3">' . $vehicleResult['BEZ1'] . '</th>
+			</tr>		
+		</thead>
+		
+		<tbody>
+			<tr>
+				<td>Produkt</td><td>Mapco</td><td>Bild</td>
+			</tr>
+		</tbody>
+	</table>
+';
