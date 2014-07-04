ab584f8124beee0fccdd8c3c80cfa6e28bb64640
mapco <rlange@mapco.de>
2014-07-04 16:19:21 +0200

    add view catalog

--- APIs/catalog/CatalogCreate.php
+++ APIs/catalog/CatalogCreate.php
@@ -1,3 +1,37 @@
 <?php
 
+/***
+ * @author: rlange@mapco.de
+ * Catalog API
+ *
+ * @params
+ * - $submitTypes
+ * -- language_id, vehicle_id
+ *
+ *
+ *******************************************************************************/
+include("../functions/cms_core.php");
+include('../functions/array_to_xml.php');
+
+$templateLoad = 'templates/template_';
+
+// keep post submit
+$post = $_POST;
+
+if (isset($post['action']) && $post['action'] == 'showCatalog')
+{
+    $data = array();
+    $data['from'] = 'vehicles_de';
+    $data['select'] = '*';
+    $data['where'] = "
+id_vehicle = " . $post["vehicle_id"];
+    $vehicleResult = SQLSelect($data['from'], $data['select'], $data['where'], 0, 0, 1, 'shop', __FILE__, __LINE__);
+//$vehiclesXml = ArrayToXml::createXML('catalogVehicles', $vehicleResult);
+//echo $vehiclesXml->saveXML();
+
+    include($templateLoad . 'car.php');
+
+    echo '<showCatalog><![CDATA[' . $templateCar . ']]></showCatalog>';
+}
+
 
--- mapco_shop_de/backend_interna_catalog.php
+++ mapco_shop_de/backend_interna_catalog.php
@@ -1,41 +1,45 @@
 <?php
 
-//	Header
+// Header
 include("config.php");
 include("templates/" . TEMPLATE_BACKEND . "/header.php");
 
-//	Content
+// Content
 ?>
-<script type="text/javascript">
+    <script type="text/javascript">
 
+        /**
+         * show
+         */
+        function showCatalog()
+        {
+            var $post_data = new Object();
+            $post_data['API'] = "catalog";
+            $post_data['APIRequest'] = "CatalogCreate";
+            $post_data['action'] = "showCatalog";
+            $post_data['vehicle_id'] = 1145;
+            $.post('<?php echo PATH;?>soa2/', $post_data, function($data) {
+                wait_dialog_show();
+                try {$xml = $($.parseXML($data));} catch($err) {show_status2($err.message); return;}
+                if ($xml.find('Ack').text() != 'Success') {show_status2($data); return;}
 
-	/**
-     * show
-     */
-	function showCatalog()
-    {
-        var $post_data = new Object();
-        $post_data['API'] = "catalog";
-        $post_data['APIRequest'] = "CatalogCreate";
-        $post_data['action'] = "showCatalog";
-        $.post('<?php echo PATH;?>soa2/', $post_data, function($data) {
-            wait_dialog_show();
-            try {$xml = $($.parseXML($data));} catch($err) {show_status2($err.message); return;}
-            if ($xml.find('Ack').text() != 'Success') {show_status2($data); return;}
-        });
-    }
-</script>
+                $('#showCatalog').append($xml.find('showCatalog').text());
+                wait_dialog_hide();
+            });
+        }
+    </script>
 
 <?php
-//	Breadcrumbs
+// Breadcrumbs
 echo '
-	<div id="breadcrumbs" class="breadcrumbs">
-		<a href="backend_index.php">Backend</a>
-		&#187; <a href="">Catalog</a>
-		&#187; Dashboard
-	</div>
-	<h1>Catalog - Dashboard</h1>';
+<div id="breadcrumbs" class="breadcrumbs">
+<a href="backend_index.php">Backend</a>
+&#187; <a href="">Catalog</a>
+&#187; Dashboard
+</div>
+<h1>Catalog - Dashboard</h1>
+<div id="showCatalog"></div>';
 
-//	Footer
+// Footer
 echo '<script type="text/javascript">showCatalog(' . $get['orderBy'] . ');</script>';
 include("templates/" . TEMPLATE_BACKEND . "/footer.php");
